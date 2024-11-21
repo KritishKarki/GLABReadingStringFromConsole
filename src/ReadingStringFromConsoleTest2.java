a7f2c872793c2ee4236ffb38f24e150e6b1d1ac0
KritishKarki <kritish.jung@gmail.com>
2024-11-20 22:43:08 -0500

    This is my first commit for Guided Lab 303.3.2: Reading a String From the Console

--- src/ReadingStringFromConsoleTest2.java
+++ src/ReadingStringFromConsoleTest2.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ReadingStringFromConsoleTest2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a character: ");
+        String s = input.nextLine();
+        char ch = s.charAt(0);
+        System.out.println("The character entered is: " + ch);
+    }
 }
